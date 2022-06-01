fd4edc58689786c3e21a0a183a1d5f62352a7d42
a <19371836+insoxin@users.noreply.github.com>
2022-06-01 16:53:43 +0800

    1

---
+++ Init.bat
@@ -0,0 +1,30 @@
+@echo off
+echo 正在创建桌面快捷方式
+
+set Program=%cd%\Bin\QQScreenShot.exe
+
+set LnkName=QQ截图
+
+set WorkDir=%cd%
+
+set Desc=QQScreenShot
+ 
+if not defined WorkDir call:GetWorkDir "%Program%"
+(echo Set WshShell=CreateObject("WScript.Shell"^)
+echo strDesKtop=WshShell.SpecialFolders("DesKtop"^)
+echo Set oShellLink=WshShell.CreateShortcut(strDesKtop^&"\%LnkName%.lnk"^)
+echo oShellLink.TargetPath="%Program%"
+echo oShellLink.WorkingDirectory="%WorkDir%\Bin"
+echo oShellLink.WindowStyle=1
+echo oShellLink.Description="%Desc%"
+echo oShellLink.Save)>makelnk.vbs
+echo 桌面快捷方式创建成功！ 
+echo www.x6d.com
+makelnk.vbs
+del /f /q makelnk.vbs
+exit
+goto :eof
+:GetWorkDir
+set WorkDir=%~dp1
+set WorkDir=%WorkDir:~,-1%
+goto :eof
